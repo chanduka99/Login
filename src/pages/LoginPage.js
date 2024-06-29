a25b7e01e1a980b410b3cf9af9923f784d9c6849
KChanduka <chanduka404@gmail.com>
2024-06-29 11:39:58 +0530

    login section

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import circle from "../assets/Circles.svg";
+import work from "../assets/WORK360.svg";
 
 function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div className="bg-[rgba(0,41,69,0.1)] h-[100vh] w-full relative overflow-clip">
+      <div className="absolute right-1">
+        <img src={circle} alt="circle" className="" />
+      </div>
+      <div className="flex h-[100vh] place-items-center">
+        {/* App360 logo and quote */}
+        <div className="ml-[15vh]  w-full">
+          <img src={work} alt="work360" className="w-[50vh] " />
+          <p className="mt-10 text-xl font-normal">
+            Now your companies & employees are in one <br /> place and always
+            under the control
+          </p>
+        </div>
+        {/* Login form */}
+        <div className=" flex justify-end   w-full">
+         
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -50,14 +50,6 @@ function SignUpPage() {
 
         </div>
       </div>
-      {/* <div className='absolute flex flex-col gap-8 top-1/2 left-32 items-center'>
-     <img src={work} alt='work360' />
-     <p className='w-2/3 content-center'>Now your companies & employees are in one place  and always under the control</p>
-    </div>
-    <div className='h-screen bg-slate-300 w-full flex justify-end  items-end'>
-     <img src={circle} alt='circle' className='h-screen' />
-    </div>
-    <div className='bg-blue-400 absolute flexCenter w-1/2 lg:lg:right-20 lg:top-1/2'>Here your log in UI </div> */}
     </div>
   );
 }
